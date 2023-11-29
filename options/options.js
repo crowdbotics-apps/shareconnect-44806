8fc9d43a52f169c7d38264f792d7394e4a843b2b
Crowdbotics <team@crowdbotics.com>
2023-11-29 23:27:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shareconnect-44806",
+  url: "https://shareconnect-44806.botics.co",
+  api: "https://shareconnect-44806.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
